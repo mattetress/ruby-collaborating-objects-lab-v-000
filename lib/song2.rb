fc97e61a89aaf05b035985964e0a360f68c102cf
Matt Etress <mattetress@me.com>
2018-10-12 01:48:20 +0000

    automatically backed up by learn

--- lib/song2.rb
+++ lib/song2.rb
@@ -1,12 +1,20 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
 
   def self.new_by_filename(filename)
-    artist
+    artist_name = filename.split(" - ")[0]
+    song_name = filename.split(" - ")[1]
+
+    song = Song.new(song_name)
+    song.artist = artist_name
+    song
+  end
+
+  def artist()
 
 
 end
