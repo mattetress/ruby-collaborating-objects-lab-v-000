8c44424e9dfdfa66770e6fd09913abc9b5133e43
Matt Etress <mattetress@me.com>
2018-10-12 01:34:35 +0000

    automatically backed up by learn

--- lib/mp3_importer2.rb
+++ lib/mp3_importer2.rb
@@ -6,8 +6,13 @@ class MP3Importer
   end
 
   def files
-    file_list = Dir["#{path}""/*.mp3"]
-    
+    file_list = Dir["#{path}/*.mp3"]
+    filenames = []
+    file_list.each do |file|
+      parts = file.split("/")
+      filenames << parts[parts.size - 1]
+    end
+    filenames
   end
 
   def import
