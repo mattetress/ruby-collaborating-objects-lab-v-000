045bd9439a6744395f615b98cb4d3af896cb43ab
Matt Etress <mattetress@me.com>
2018-10-12 01:44:18 +0000

    automatically backed up by learn

--- lib/song2.rb
+++ lib/song2.rb
@@ -1,11 +1,12 @@
 class Song
-  attr_accessor :artist, :name
+  attr_accessor :name
 
   def initialize(name)
     @name = name
   end
 
-  
+  def self.new_by_filename(filename)
+    artist
 
 
 end
