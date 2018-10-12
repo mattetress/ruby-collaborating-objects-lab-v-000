7353dcc7b77a5e601b56dc12ba09845ad1654a16
Matt Etress <mattetress@me.com>
2018-10-12 01:55:48 +0000

    automatically backed up by learn

--- lib/artist2.rb
+++ lib/artist2.rb
@@ -1,3 +1,21 @@
 class Artist
+  attr_reader :name
+  @@all = []
+
+  def self.all
+    @@all 
+  end
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    self.all.detect {|a| a.name == artist_name} || self.new(artist_name)
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|a| a.name == name}
+    
 
 end
